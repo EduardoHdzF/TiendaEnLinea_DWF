d4f35825a245dec34b034623d4968d3699e5fc2f
Eduardo Hernandez F <eduardohf@ciencias.unam.mx>
2024-04-24 09:02:45 -0600

    "creación componente detalles de producto"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { LoginComponent } from './module/authentication/login/login.component';
 import { SecuredComponent } from './module/authentication/secured/secured.component';
 import { authenticationGuard } from './module/authentication/_guard/authentication.guard';
 import { ProductComponent } from './module/product/component/product/product.component';
+import { ProductDescriptionComponent } from './module/product/component/product-description/product-description.component';
 
 const routes: Routes = [
   {path: "category", component: CategoryComponent},
   {path: "product", component: ProductComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'secured', component: SecuredComponent, canActivate : [authenticationGuard]}
+  {path: 'secured', component: SecuredComponent, canActivate : [authenticationGuard]},
+  {path: 'description', component: ProductDescriptionComponent}
 
 ];
 
--- src/app/module/product/product.module.ts
+++ src/app/module/product/product.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { CategoryComponent } from './component/category/category.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ProductComponent } from './component/product/product.component';
+import { ProductDescriptionComponent } from './component/product-description/product-description.component';
 
 
 
 @NgModule({
   declarations: [
     CategoryComponent,
-    ProductComponent
+    ProductComponent,
+    ProductDescriptionComponent
   ],
   imports: [
     CommonModule,

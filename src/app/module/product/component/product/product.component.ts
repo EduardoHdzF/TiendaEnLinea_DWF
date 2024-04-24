ef6cb37c98f83537b07ea775247927b467e85e0e
EduardoHdzF <90948381+EduardoHdzF@users.noreply.github.com>
2024-04-24 08:01:42 -0600

    actualización del componente producto

--- src/app/module/product/component/product/product.component.ts
+++ src/app/module/product/component/product/product.component.ts
@@ -5,6 +5,10 @@ import { ProductService } from '../../_service/product.service';
 import { DtoProductList } from '../../_dto/dto-product-list';
 import { Category } from '../../_model/category';
 import { CategoryService } from '../../_service/category.service';
+import { Product } from '../../_model/product';
+import { ProductImage } from '../../_model/product-image';
+import { ProductImageService } from '../../_service/product-image.service';
+import { NgxPhotoEditorService } from 'ngx-photo-editor';
 
 declare var $: any; // JQuery
 
@@ -16,6 +20,7 @@ declare var $: any; // JQuery
 export class ProductComponent {
 
   products: DtoProductList[] = []; // product list
+  productToUpdate: number = 0; // product id
 
   categories: Category[] = []; // category list
 
@@ -24,9 +29,9 @@ export class ProductComponent {
     product: ["", [Validators.required]],
     gtin: ["", [Validators.required, Validators.pattern('^[0-9]{13}$')]],
     description: ["", [Validators.required]],
-    price: ["", [Validators.required, Validators.pattern('^[0-9]*$')]],
-    stock: ["", [Validators.required, Validators.pattern('^[0-9]*$')]],
-    category_id: ["", [Validators.required]],
+    price: [0, [Validators.required, Validators.pattern('^[0-9]*$')]],
+    stock: [0, [Validators.required, Validators.pattern('^[0-9]*$')]],
+    category_id: [0, [Validators.required]],
   });
 
   submitted = false; // Form submitted
@@ -36,11 +41,14 @@ export class ProductComponent {
   constructor(
     private categoryService: CategoryService,
     private productService: ProductService,
+    private productImageService: ProductImageService,
     private formBuilder: FormBuilder,
+    private service: NgxPhotoEditorService
   ){}
 
   ngOnInit(){
     this.getProducts();
+    this.getActiveCategories();
   }
 
   disableProduct(id: number){
@@ -107,6 +115,14 @@ export class ProductComponent {
     if(this.form.invalid) return;
     this.submitted = false;
 
+    if(this.productToUpdate == 0){
+      this.onSubmitCreate();
+    }else{
+      this.onSubmitUpdate();
+    }
+  }
+
+  onSubmitCreate(){
     this.productService.createProduct(this.form.value).subscribe({
       next: (v) => {
         this.swal.successMessage(v.body!.message); // show message
@@ -120,11 +136,55 @@ export class ProductComponent {
     });
   }
 
+  onSubmitUpdate(){
+    this.productService.updateProduct(this.form.value, this.productToUpdate).subscribe({
+      next: (v) => {
+        this.swal.successMessage(v.body!.message); // show message
+        this.getProducts(); // reload products
+        this.hideModalForm(); // close modal
+        this.productToUpdate = 0; // reset product to update
+      },
+      error: (e) => {
+        console.error(e);
+        this.swal.errorMessage(e.error!.message); // show message
+      }
+    });
+  }
+
+  updateProduct(gtin: string){
+    $("#productModal").modal("hide");
+    this.productService.getProduct(gtin).subscribe({
+      next: (v) => {
+        let product = v.body!;
+
+        this.productToUpdate = product.product_id;
+
+        this.form.reset();
+        this.submitted = false;
+
+        this.form.controls['product'].setValue(product.product);
+        this.form.controls['gtin'].setValue(product.gtin);
+        this.form.controls['price'].setValue(product.price);
+        this.form.controls['stock'].setValue(product.stock);
+        this.form.controls['category_id'].setValue(product.category_id);
+        this.form.controls['description'].setValue(product.description);
+
+        $("#modalForm").modal("show");
+      },
+      error: (e) => {
+        console.log(e);
+        this.swal.errorMessage(e.error!.message); // show message
+      }
+    });
+  }
+
+  // modals 
+
   showModalForm(){
     $("#modalForm").modal("show");
     this.form.reset();
     this.submitted = false;
-    this.getCategories();
+    this.productToUpdate = 0;
   }
 
   hideModalForm(){
@@ -133,8 +193,8 @@ export class ProductComponent {
 
   // catalogues 
 
-  getCategories(){
-    this.categoryService.getCategories().subscribe({
+  getActiveCategories(){
+    this.categoryService.getActiveCategories().subscribe({
       next: (v) => {
         this.categories = v.body!;
       },

5d64934f4697b98918ecfbbbcd2319c0f29432a8
FEDORA32 <keniaromero@ciencias.unam.mx>
2024-05-22 21:42:18 -0600

    intergación visto agregar al carrito

--- src/app/module/invoice/component/cart/cart.component.ts
+++ src/app/module/invoice/component/cart/cart.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { Cart } from '../../_model/cart';
 import { CartService } from '../../_service/cart.service';
+import { Cart } from '../../_model/cart';
 import { ProductDescriptionComponent } from '../../../product/component/product-description/product-description.component';
 import { DtoCartDetails } from '../../_dto/dto-cart-details';
+import { SwalMessages } from '../../../commons/_model/swal-messages';
 
 @Component({
   selector: 'app-cart',
@@ -10,5 +11,42 @@ import { DtoCartDetails } from '../../_dto/dto-cart-details';
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
+
+  cart: DtoCartDetails[] = [];
+
+  cartItem: Cart[] = []; // Category list
+
+  gtin: string = "";
+
+  quantity: number = 1;
+
+  swal: SwalMessages = new SwalMessages(); // swal messages
+
+  constructor(
+    private cartService: CartService,
+  ){}
+
+  ngOnInit(){
+    this.getCart();
+  }
+
+  getCart(){
+    this.cartService.getCart().subscribe({
+      next: (v) => {
+        this.cart = v.body!;
+      },
+      error: (e) => {
+        console.log(e);
+        this.swal.errorMessage(e.error!.message); // show message
+      }
+    });
+  }
+
+  // addToCart(gtin: string, quantity: number){
+  //   const cartItem: Cart = { gtin, quantity };
+  //   this.cartService.addToCart(cartItem).subscribe(
+
+  //   )
+  // }
 
 }
